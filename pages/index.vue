a6a7b701b5f189def9f1c7adfcfed984f04ca0f3
GWjun <kb865900@gmail.com>
2025-02-04 20:43:24 +0900

    feat: pinia

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Main Page</h2>
+    <div>Count: {{ counter.count }}</div>
+    <button @click="counter.increment">add count</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const counter = useCounterStore()
+</script>
